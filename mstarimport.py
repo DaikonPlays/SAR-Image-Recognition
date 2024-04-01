c86f1e83d55d2c000d36c6349e12c8fd0eaf5b34
DaikonPlays <kevinyan904@gmail.com>
2024-04-01 16:44:32 -0700

    start of model training, not working though

--- mstarimport.py
+++ mstarimport.py
@@ -1,9 +1,11 @@
 import os
 import torch
-from torchvision import transforms, datasets
-from torch.utils.data import DataLoader, ConcatDataset
+from torchvision import transforms, datasets, models
+from torch.utils.data import DataLoader, ConcatDataset, random_split
 import matplotlib.pyplot as plt
 import numpy as np
+import torch.nn as nn
+import torch.optim as optim
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)), 
@@ -22,7 +24,7 @@ for deg_val in degree_dirs :
     datasets_list.append(dataset)
 concatenated_dataset = ConcatDataset(datasets_list)
 dataloader = DataLoader(concatenated_dataset, batch_size=32, shuffle=True)
-print(len(concatenated_dataset))
+# print(len(concatenated_dataset))
 def imshow(img, mean, std):
     img = img.numpy().transpose((1, 2, 0))
     img = std * img + mean  
@@ -32,8 +34,40 @@ def imshow(img, mean, std):
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 for images, labels in dataloader:
-    imshow(images[0], mean, std)
+    imshow(images[1], mean, std)
     print(labels)
+    # print("hi")
     break
-    
+total_size = len(concatenated_dataset)
+train_size = int(total_size * 0.8)  
+test_size = total_size - train_size 
+train_dataset, test_dataset = random_split(concatenated_dataset, [train_size, test_size])
+model = models.resnet18(pretrained=True)    
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+for name, param in model.named_parameters():
+    if "fc" in name:  
+        param.requires_grad = True
+    else:
+        param.requires_grad = False
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9) 
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    # running_corrects = 0
+    for images, labels in train_dataset:
+        images = images.to(device)
+        if not isinstance(labels, torch.Tensor):
+            labels = torch.tensor(labels) 
+        labels = labels.to(device)  
+        optimizer.zero_grad()
+        outputs = model(images.unsqueeze(0))
+        outputs = torch.squeeze(outputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        # running_corrects += torch.sum(preds == labels.data)
 
