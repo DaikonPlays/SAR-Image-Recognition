acd9e6e9dff04fe1aff0c85944e4c847bb852cf4
DaikonPlays <kevinyan904@gmail.com>
2024-05-22 19:37:44 -0700

    Implemented hypervectors for classification

--- test.py
+++ test.py
@@ -1,43 +1,53 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+from torchvision.datasets import MNIST
 
 # Note: this example requires the torchmetrics library: https://torchmetrics.readthedocs.io
 import torchmetrics
 from tqdm import tqdm
 
 import torchhd
-from torchhd import embeddings
 from torchhd.models import Centroid
-from torchhd.datasets.isolet import ISOLET
+from torchhd import embeddings
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using {} device".format(device))
 
-DIMENSIONS = 10000  # number of hypervector dimensions
-NUM_LEVELS = 100
+DIMENSIONS = 10000
+IMG_SIZE = 28
+NUM_LEVELS = 1000
 BATCH_SIZE = 1  # for GPUs with enough memory we can process multiple images at ones
+
+transform = torchvision.transforms.ToTensor()
+
+train_ds = MNIST("../data", train=True, transform=transform, download=True)
+train_ld = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+
+test_ds = MNIST("../data", train=False, transform=transform, download=True)
+test_ld = torch.utils.data.DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
 
 
 class Encoder(nn.Module):
-    def __init__(self, num_classes, size):
+    def __init__(self, out_features, size, levels):
         super(Encoder, self).__init__()
-        self.id = embeddings.Random(size, DIMENSIONS)
-        self.value = embeddings.Level(NUM_LEVELS, DIMENSIONS)
+        self.flatten = torch.nn.Flatten()
+        self.position = embeddings.Random(size * size, out_features)
+        self.value = embeddings.Level(levels, out_features)
 
     def forward(self, x):
-        sample_hv = torchhd.bind(self.id.weight, self.value(x))
+        x = self.flatten(x)
+        y = self.value(x)
+        print("Size of self.position.weight:", self.position.weight.size())
+        print("Size of self.value(x):", y.size())
+        sample_hv = torchhd.bind(self.position.weight, self.value(x))
         sample_hv = torchhd.multiset(sample_hv)
         return torchhd.hard_quantize(sample_hv)
 
 
-train_ds = ISOLET("../data", train=True, download=True)
-train_ld = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
-
-test_ds = ISOLET("../data", train=False, download=True)
-test_ld = torch.utils.data.DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
-
-encode = Encoder(DIMENSIONS, train_ds[0][0].size(-1))
+encode = Encoder(DIMENSIONS, IMG_SIZE, NUM_LEVELS)
 encode = encode.to(device)
 
 num_classes = len(train_ds.classes)
