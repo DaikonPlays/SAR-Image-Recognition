662f8b1723f17511d01c9cd47fb8969384b83359
srajendra001 <srajendra@ucsd.edu>
2024-08-07 20:53:39 -0700

    Updated HDC code to use higher image size

--- test.py
+++ test.py
@@ -7,64 +7,74 @@ import torchmetrics
 from tqdm import tqdm
 from torchvision import transforms, datasets, models
 import torchhd
-from torchhd.models import Centroid
+from torchhd.models import Centroid, IntRVFL
 from torchhd import embeddings
 from torch.utils.data import DataLoader, ConcatDataset, random_split
-
+import os
+import argparse as ap
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using {} device".format(device))
 
-DIMENSIONS = 10000
-IMG_SIZE = 28
+DIMENSIONS = 20000
+IMG_SIZE = 128
 NUM_LEVELS = 1000
 BATCH_SIZE = 1  
-transform = torchvision.transforms.Compose([
-    torchvision.transforms.Resize((IMG_SIZE, IMG_SIZE)),
-    transforms.Grayscale(num_output_channels=1),
-    torchvision.transforms.ToTensor()
-])
 
 class Encoder(nn.Module):
     def __init__(self, out_features, size, levels):
         super(Encoder, self).__init__()
         self.flatten = torch.nn.Flatten()
-        self.position = embeddings.Random(size * size, out_features)
-        self.value = embeddings.Level(levels, out_features)
+        if args.type == 'linear':
+            self.position = embeddings.Random(size * size, out_features)
+            self.value = embeddings.Level(levels, out_features)
+        else:
+            self.nonlinear_projection = embeddings.Sinusoid(size * size, out_features)
 
     def forward(self, x):
         x = self.flatten(x)
-        y = self.value(x)
-        # print("Size of self.position.weight:", self.position.weight.size())
-        # print("Size of self.value(x):", y.size())
-        print(y.size())
-        # print(self.position.weight.size())
-        sample_hv = torchhd.bind(self.position.weight, self.value(x))
-        sample_hv = torchhd.multiset(sample_hv)
+        if args.type == 'linear':
+            sample_hv = torchhd.bind(self.position.weight, self.value(x))
+            sample_hv = torchhd.multiset(sample_hv)
+        else:
+            sample_hv = self.nonlinear_projection(x)
         return torchhd.hard_quantize(sample_hv)
 
-transform = torchvision.transforms.ToTensor()
-MSTAR_dir = '/Users/kevinyan/Downloads/MSTAR_TargetData/';  
-dataset = datasets.ImageFolder(root=MSTAR_dir, transform=transform)
-dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-total_size = len(dataset)
-train_size = int(total_size * 0.8)  
-test_size = total_size - train_size 
-train_ds, test_ds = random_split(dataset, [train_size, test_size])
-train_ld = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
-test_ld = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
-# train_ds = MNIST("../data", train=True, transform=transform, download=True)
-# train_ld = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+# transform = torchvision.transforms.ToTensor()
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.Grayscale(num_output_channels=1),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.1307,), (0.3081,))
+])
 
-# test_ds = MNIST("../data", train=False, transform=transform, download=True)
-# test_ld = torch.utils.data.DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
+parser = ap.ArgumentParser()
+parser.add_argument('--dataset', type=str, required=True)
+parser.add_argument('--type', type=str, default='linear')
+args = parser.parse_args()
 
+if args.dataset == 'mstar':
+    MSTAR_dir = r'mstar'
+    dataset = datasets.ImageFolder(root=MSTAR_dir, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+    total_size = len(dataset)
+    train_size = int(total_size * 0.8)  
+    test_size = total_size - train_size 
+    train_ds, test_ds = random_split(dataset, [train_size, test_size])
+    train_ld = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+    test_ld = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
+    num_classes = len(dataset.classes)
+else:
+    train_ds = MNIST("../data", train=True, transform=transform, download=True)
+    train_ld = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+    test_ds = MNIST("../data", train=False, transform=transform, download=True)
+    test_ld = torch.utils.data.DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
+    num_classes = len(train_ds.classes)
 
 encode = Encoder(DIMENSIONS, IMG_SIZE, NUM_LEVELS)
 encode = encode.to(device)
 
-num_classes = 8
-model = Centroid(DIMENSIONS, num_classes)
+model = Centroid(in_features=DIMENSIONS, out_features=num_classes)
 model = model.to(device)
 
 with torch.no_grad():
